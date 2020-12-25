727291e53285b3100e784ecd0d090828388333ac
= <saleh.majeed8@gmail.com>
2020-12-26 00:23:47 +0530

    done currying exercise

--- js/curry_exercis.js
+++ js/curry_exercis.js
@@ -7,32 +7,25 @@ const split = _.curry((delimiter, string) => string.split(delimiter))
 // Exercise 1
 //==============
 
-const words = str => str.split(' ');
+const words = split(' ')
+console.log(words("Jingle bells Batman smells"), ['Jingle', 'bells', 'Batman', 'smells'])
 
-
-QUnit.test("Ex1: split", assert => {
-  assert.deepEqual(words("Jingle bells Batman smells"), ['Jingle', 'bells', 'Batman', 'smells'])
-})
 
 
 // Exercise 1a
 //==============
 //use map to make a new words fn that not only works on 1 string, but on an array of strings.
 
-const sentences =  _.map(words);
+const sentences = _.map(words);
+console.log(sentences(["Jingle bells Batman smells", "Robin laid an egg"]), [['Jingle', 'bells', 'Batman', 'smells'], ['Robin', 'laid', 'an', 'egg']]);
 
-QUnit.test("Ex1a: map/split", assert => {
-  assert.deepEqual(sentences(["Jingle bells Batman smells", "Robin laid an egg"]), [['Jingle', 'bells', 'Batman', 'smells'], ['Robin', 'laid', 'an', 'egg']]);
-})
 
 
 // Exercise 2
-//=============
-const filterQs = _.filter(_.test(/q/ig));
+//==============
+const filterQs = _.filter(_.test(/q/ig))
+console.log(filterQs(['quick', 'camels', 'quarry', 'over', 'quails']), ['quick', 'quarry', 'quails']);
 
-QUnit.test("Ex2: filter", assert => {
-  assert.deepEqual(filterQs(['quick', 'camels', 'quarry', 'over', 'quails']), ['quick', 'quarry', 'quails']);
-})
 
 
 // Exercise 3
@@ -43,10 +36,10 @@ const _keepHighest = (x,y) => x >= y ? x : y // <- leave be
 
 // TODO: rewrite max in its "simplest" form
 const max = _.reduce(_keepHighest, 0)
+const equal = (x, y) => x == y
 
-QUnit.test("Ex3: max", assert => {
-  assert.equal(max([323,523,554,123,5234]), 5234);
-})
+console.log(equal(max([323,523,554,123,5234]), 5234));
+
 
 
 // Bonus 1:
@@ -54,17 +47,13 @@ QUnit.test("Ex3: max", assert => {
 // wrap array's built in slice to be functional and curried like ramda fn's.
 // //[1,2,3].slice(0, 2)
 
-const slice = _.curry((start, end, xs) => xs.slice(start, end))
+const slice = _.curry((x, y, arr) => arr.slice(x, y))
+console.log(slice(1)(3)(['a', 'b', 'c']), ['b', 'c']);
 
-QUnit.test("Bonus 1", assert => {
-  assert.deepEqual(slice(1)(3)(['a', 'b', 'c']), ['b', 'c']);
-})
 
 // Bonus 2:
 // ============
 // use slice to define a function take() that takes n elements from an array. make it curried
-const take = slice(0)
+const take = _.curry((n, arr) => arr.slice(0, n))
+console.log(take(2)(['a', 'b', 'c']), ['a', 'b']);
 
-QUnit.test("Bonus 2", assert => {
-  assert.deepEqual(take(2)(['a', 'b', 'c']), ['a', 'b']);
-})
